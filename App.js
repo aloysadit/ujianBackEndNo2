f7144e703fe9616c671e12ef50101ed81c5d6272
aloysadit <aloysiusaditama@gmail.com>
2018-07-11 13:51:40 +0700

    second commit

---
+++ App.js
@@ -0,0 +1,47 @@
+var express = require('express')
+var app = express()
+
+var mo = require('mongodb').MongoClient;
+var url = 'mongodb://root:1234@localhost:27017/ujian';
+
+var os = require('os')
+var namaCPU =  os.hostname()
+var osTipe = os.type();
+var osPlatform = os.platform()
+var osRilis = os.release()
+var ramSisa = os.freemem()
+var ramTotal = os.totalmem()
+
+// --mongodb-- //
+mo.connect(url, (error,data)=>{
+    console.log('Terhubung ke MongoDB!')
+})
+
+
+app.get('/data',(req,res)=>{
+    mo.connect(url,(error,data)=>{
+        var collection = data.collection('datacpu');
+        collection.find({}).toArray((error,hasil)=>{
+            console.log(hasil);
+            res.send(hasil);
+        })
+    })
+})
+
+
+app.post('/data',(req,res)=>{
+    mo.connect(url,(error,data)=>{
+        var datas = {namacpu: namaCPU, tipe: osTipe, platform: osPlatform , rilis: osRilis , ramSisa: ramSisa, ramTotal: ramTotal};
+        var collection = data.collection('datacpu');
+        collection.insert(datas, (error,hasil)=>{
+            console.log(hasil);
+            res.send(hasil);
+        })
+    })
+})
+
+
+
+app.listen(3210,()=>{
+    console.log('terhubung @port 3210')
+})
